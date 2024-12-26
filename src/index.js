7f5d6fc4417f88a602a48c155f4d36376ccb1a00
anagha kishan <anagha.kishan@sts.co.in>
2024-12-26 21:31:36 +0530

    portfolio

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/anagha-portfolio">
     <App />
     </BrowserRouter>
   </React.StrictMode>
