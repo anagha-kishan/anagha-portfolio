a246370b37b9e94628046b17b5057c6c1c4a84f2
anagha kishan <anagha.kishan@sts.co.in>
2024-12-26 21:11:13 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Import BrowserRouter
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -7,15 +7,15 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path='/' element = {<Layout />} >
-        <Route index element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-      </Route>
-    </Routes>
-    </>
+    <BrowserRouter basename="/anagha-portfolio"> {/* Set basename for GitHub Pages */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
