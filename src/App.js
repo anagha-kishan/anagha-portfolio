2e3eff5b103c9ae61ee779c52b07a67bf139ba5d
anagha kishan <anagha.kishan@sts.co.in>
2025-04-03 12:48:51 +0530

    deployment changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Import BrowserRouter
+import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -7,15 +7,15 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <BrowserRouter basename="/anagha-portfolio"> {/* Set basename for GitHub Pages */}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+    <Routes>
+      <Route path='/' element = {<Layout />} >
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Route>
+    </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/anagha-portfolio"> */}
-    <BrowserRouter>
+    <BrowserRouter basename="/anagha-portfolio">
     <App />
     </BrowserRouter>
   </React.StrictMode>
